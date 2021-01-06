c2a2b1dd05e7701e9152ad0653a492375d6f18b2
Herman W. Jacobsen <herman@wjacobsen.dk>
2021-01-06 14:09:47 +0000

    Create webmin.sh

---
+++ webmin.sh
@@ -0,0 +1,5 @@
+apt-get update
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
+apt-get update
+apt-get install webmin
