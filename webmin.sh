b5072650a4bdbe7e221476d6e9865ed6f01dc878
Herman W. Jacobsen <herman@wjacobsen.dk>
2021-01-06 14:13:57 +0000

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,4 +1,5 @@
 apt-get update
+sed -i '/webmin.com/d' /etc/apt/sources.list
 echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
 wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
 apt-get update
